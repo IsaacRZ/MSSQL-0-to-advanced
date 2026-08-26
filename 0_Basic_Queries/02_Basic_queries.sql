9fa625b628e9911f8bc6d9bcc0b0b010eafaf565
Isaac Rodriguez <isaac_024@hotmail.com>
2026-08-26 11:28:45 -0600

    code: Practice to basic queries

--- 0_Basic_Queries/02_Basic_queries.sql
+++ 0_Basic_Queries/02_Basic_queries.sql
@@ -1,11 +1,33 @@
 USE sql_practica;
 
--- SELECT
--- FROM
--- columns
--- table;
+
+-- Comment
+
+/* 
+    SELECT
+    FROM
+    columns
+    table;
+ */
+
 
 -- All columns: *
 SELECT * FROM games;
 
+-- Select only title and year
+SELECT title, year FROM games;
 
+-- Limit the results, top 3 results
+SELECT TOP 3 title,year FROM games;
+
+/*
+Practice:
+*/
+
+-- Select all columns from the games table.
+
+-- Select only the title of all video games.
+
+-- Select the title and the year of all video games.
+
+-- Select the title, the developer, and the playtime hours.   
